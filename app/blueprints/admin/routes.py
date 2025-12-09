453c5be6387a7c726cda4bd33b03fd790a57eadd
MrBlindMouse <neljohan1206@gmail.com>
2025-12-09 08:29:00 +0200

    Added admin.users
    View and manage user roles from admin panel

--- app/blueprints/admin/routes.py
+++ app/blueprints/admin/routes.py
@@ -3,12 +3,15 @@ from flask import (
     render_template,
     redirect,
     url_for,
-    flash
+    flash,
+    abort
 )
 import jinja2, os
 from . import bp
 
 from flask_login import current_user, login_required
+
+from functools import wraps
 
 from app.models import models
 from app.utils.site_config import invalidate_config_cache
@@ -18,16 +21,23 @@ from . import forms
 import json
 
 
-
+def admin_required(f):
+    @wraps(f)
+    @login_required
+    def decorated_view(*args, **kwargs):
+        if current_user.role != 'ADMIN':
+            abort(403)
+        return f(*args, **kwargs)
+    return decorated_view
 
 @bp.route("/")
-@login_required
+@admin_required
 def index():
     return render_template("core/index.html")
 
 @bp.route("/settings", methods=["GET", "POST"])
 @bp.route("/settings/<addon_id>", methods=["GET", "POST"])
-@login_required
+@admin_required
 def settings(addon_id = None):
     setup = models.get_config(addon_id = addon_id)
     formClass = forms.dynamic_form(setup)
@@ -54,14 +64,56 @@ def settings(addon_id = None):
         addons = addons
     )
 
+@bp.route('/users')
+@admin_required
+def users():
+    data = models.User.get()
+    users = [user for user in data if user.role == 'CLIENT']
+    admins = [user for user in data if user.role == 'ADMIN']
+    return render_template(
+        "core/users.html",
+        users = users,
+        admins = admins
+    )
+
+@bp.route('/set-user/<int:id>/<direction>', methods=["POST"])
+@admin_required
+def set_user_role(id, direction):
+    users = models.User.get(id=id)
+    if not users:
+        abort(404, description="User not found")
+    user = users[0]
+    if user.role == 'CLIENT' and direction == 'ADD':
+        user.role = 'ADMIN'
+        user.update()
+        flash(f"{user.name} is now an Admin", "success")
+    elif user.role == 'ADMIN' and direction == 'REMOVE':
+        user.role = 'CLIENT'
+        user.update()
+        flash(f"{user.name} as been removed as Admin", "success")
+    else:
+        flash("Invalid operation!", "danger")
+    
+    return redirect(url_for('admin.users'))
+
+@bp.route('/products')
+def products():
+    return render_template(
+        "core/products.html"
+    )
 
 @bp.route("/suppliers")
-@login_required
+@admin_required
 def suppliers():
     return render_template("core/suppliers.html")
 
-@bp.route("/payments")
-@login_required
-def payments():
-    return render_template("core/payments.html")
+@bp.route("/payment-processors")
+@admin_required
+def payment_processors():
+    return render_template("core/payment_processors.html")
+
+@bp.route("/addons")
+@admin_required
+def addons():
+    return render_template("core/addons.html")
 
